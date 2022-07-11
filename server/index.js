60ff5cc7a64f0af31508750cf112c824ea133fc8
johnscollins98 <johnscollins98@gmail.com>
2022-07-11 12:37:24 +0100

    swap back to npm to see if that works

--- server/index.js
+++ server/index.js
@@ -24,4 +24,5 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3001, () => console.log('Server running on port 3001'));
+const port = process.env.PORT;
+server.listen(port, () => console.log(`Server running on port ${port}`));

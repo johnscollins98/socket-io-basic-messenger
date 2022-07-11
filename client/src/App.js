fb996a523b4434c8b21460b141356792b7ce609e
John Collins <johnscollins98@gmail.com>
2022-07-11 01:25:25 +0100

    create basic messenger app

---
+++ client/src/App.js
@@ -0,0 +1,91 @@
+import { useEffect, useState, useCallback } from 'react';
+import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:3001');
+
+function App() {
+  const [name, setName] = useState(localStorage.getItem('name') ?? '');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    setMessages([{ name, message, me: true }, ...messages]);
+    setMessage('');
+    localStorage.setItem('name', name);
+    socket.emit('SEND_MESSAGE', { name, message });
+  };
+
+  const receiveMessage = useCallback(
+    ({ message, name }) => {
+      setMessages([{ name, message, me: false }, ...messages]);
+    },
+    [messages]
+  );
+
+  useEffect(() => {
+    socket.on('RECEIVE_MESSAGE', receiveMessage);
+  }, [receiveMessage]);
+
+  return (
+    <div
+      className="App p-3 d-flex flex-column container-fluid"
+      style={{ height: '100vh', overflow: 'hidden' }}
+    >
+      <div className="name mb-3">
+        <label htmlFor="your-name" className="form-label">
+          Enter your name:{' '}
+        </label>
+        <input
+          placeholder="Name..."
+          type="text"
+          id="your-name"
+          name="your-name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className="form-control"
+        />
+      </div>
+      <div
+        className="messages flex-grow-1"
+        style={{ display: 'flex', flexDirection: 'column-reverse', overflow: 'auto' }}
+      >
+        {messages.map((m, i) => (
+          <div
+            className="card mb-1"
+            style={{
+              width: 'fit-content',
+              alignSelf: m.me ? 'flex-start' : 'flex-end',
+              background: m.me ? 'rgb(0, 132, 255)' : undefined,
+              color: m.me ? 'white' : undefined,
+            }}
+            key={i}
+          >
+            <div className="card-body">
+              <h5 className="card-title">{m.name}</h5>
+              <p className="card-text">{m.message}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <form onSubmit={sendMessage} className="mb-3 mt-3">
+        <div className="input-group message">
+          <input
+            placeholder="Message..."
+            type="text"
+            id="messageInput"
+            name="messageInput"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            className="form-control"
+          />
+          <button type="submit" className="btn btn-primary">
+            Send
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const http = require("http")
+const { Server } = require("socket.io")
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log(`User connnected ${socket.id}`);
+
+  socket.on('SEND_MESSAGE', ({ message, name }) => {
+    socket.broadcast.emit('RECEIVE_MESSAGE', { message, name })
+  })
+})
+
+server.listen(3001, () => console.log("Server running on port 3001"))

f8c75c75f579ca0347a1dd17260b835453b93bf9
John Collins <johnscollins98@gmail.com>
2022-07-11 08:05:30 +0100

    serve built react from express backend

--- server/index.js
+++ server/index.js
@@ -1,25 +1,27 @@
-const express = require("express");
-const http = require("http")
-const { Server } = require("socket.io")
-const cors = require("cors");
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(cors());
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
 });
 
 io.on('connection', (socket) => {
   console.log(`User connnected ${socket.id}`);
 
   socket.on('SEND_MESSAGE', ({ message, name }) => {
-    socket.broadcast.emit('RECEIVE_MESSAGE', { message, name })
-  })
-})
+    socket.broadcast.emit('RECEIVE_MESSAGE', { message, name });
+  });
+});
 
-server.listen(3001, () => console.log("Server running on port 3001"))
+server.listen(3001, () => console.log('Server running on port 3001'));
